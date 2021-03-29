134d515e744cb3e384b2ec05f74e566c61989140
DINNERman <shrwnwilliams@gmail.com>
2021-03-29 17:45:35 -0400

    roughly finished search and weather appending functionality

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,22 +1,37 @@
 var userCityInput = $("#city-search-input");
 var formInputEl = $("#city-search");
+var todaysDateEl = $("#todays-date");
+var weatherDisplay = $("#weather-display");
+var currentCity = $("#city-name");
+var currentTemp = $("#current-temperature");
+var currentHumidity = $("#current-humidity");
+var currentWind = $("#current-wind");
+var currentUvIndex = $("#current-uv");
+var today = moment();
+
+todaysDateEl.text(today.format("l"));
 
 function getCity() {
   var requestUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=" + userCityInput.val() +"&units=imperial&appid=49e97d128aa0b52b9299b5b1b5a52107";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    userCityInput.val() +
+    "&units=imperial&appid=49e97d128aa0b52b9299b5b1b5a52107";
 
   fetch(requestUrl)
     .then(function (response) {
-      return response.json();
+      if (response.ok) return response.json();
     })
     .then(function (data) {
       console.log(data);
+      currentTemp.text(data.main.temp + "°F");
+      currentHumidity.text(data.main.humidity + "%");
+      currentWind.text(data.wind.speed + "mph");
     });
 }
 
-
 formInputEl.on("submit", function (event) {
-    event.preventDefault();
-    console.log(userCityInput.val());
-    getCity();
+  event.preventDefault();
+  getCity();
+  weatherDisplay.removeClass("d-none");
+  currentCity.text(userCityInput.val());
 });
