652e034ba3ac1e017f0240be5a456cc1581f91c0
DINNERman <shrwnwilliams@gmail.com>
2021-03-29 16:50:15 -0400

    got form to sumbit, prevent default and log the user's input as well as got the api to successfully log the data for Atlanta

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+var userCityInput = $("#city-search-input");
+var formInputEl = $("#city-search");
+
+function getCity() {
+  var requestUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=atlanta&units=imperial&appid=49e97d128aa0b52b9299b5b1b5a52107";
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+getCity();
+
+formInputEl.on("submit", function (event) {
+  event.preventDefault();
+  console.log(userCityInput.val());
+});
