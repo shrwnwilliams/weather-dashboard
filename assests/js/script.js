4169b9bd8e8ae6038ddcf9c4e75b177ccd372a26
DINNERman <shrwnwilliams@gmail.com>
2021-03-30 19:24:07 -0400

    continue work on rendering 5 day forecast onto the screen

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,12 +3,16 @@ $(document).ready(function () {
   var formInputEl = $("#city-search");
   var todaysDateEl = $("#todays-date");
   var weatherDisplay = $("#weather-display");
+  var fiveDayDisplay = $("#5-day-forecast");
   var currentCity = $("#city-name");
   var currentTemp = $("#current-temperature");
   var currentHumidity = $("#current-humidity");
   var currentWind = $("#current-wind");
   var currentUvIndex = $("#current-uv");
   var pastSearchList = $("#past-searches");
+  var dayOneHigh = $("#1-day-high");
+  var dayOneLow = $("#1-day-low");
+  var dayOneHumidity = $("#1-day-humid");
   var today = moment();
   var searchHistory = JSON.parse(localStorage.getItem("history")) || [];
 
@@ -29,6 +33,7 @@ $(document).ready(function () {
         currentCity.text(data.name);
         // console.log(data.coord.lat);
         // console.log(data.coord.lon);
+        getFiveDay(data.coord.lat, data.coord.lon);
         getCityWeather(data.coord.lat, data.coord.lon);
         if (searchHistory.indexOf(city) === -1) {
           searchHistory.push(city);
@@ -83,16 +88,45 @@ $(document).ready(function () {
         currentUvIndex.text(data.current.uvi);
       });
   };
+  
+
+  var getFiveDay = function(lat, lon) {
+    var requestWeatherUrl =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&units=imperial&exclude=hourly,minutely&appid=49e97d128aa0b52b9299b5b1b5a52107";
+
+  fetch(requestWeatherUrl).then(function (response){
+    if (response.ok) return response.json();
+  }).then(function(data) {
+    console.log(data);
+    for (var i = 0; i < 4; i++){
+      var nextDay = $("<div>");
+      var weatherImg = $("<img>");
+      weatherImg.attr("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon +"@2x.png");
+      // formInputEl.append(weatherImg);
+
+      dayOneHigh.text(data.daily[i].temp.max + "°F");
+      dayOneLow.text(data.daily[i].temp.min + "°F");
+      dayOneHumidity.text(data.daily[i].humidity + "%")
+    }
+  })
+
+
+  };
+
 
   formInputEl.on("submit", function (event) {
     event.preventDefault();
     // getLatLong();
     getSearchValue();
     weatherDisplay.removeClass("d-none");
+    fiveDayDisplay.removeClass("d-none");
   });
 
   // storing past search list items into local storage
-  // when clicking on the lists in the bar, the weather will change to those cities
   // finding different end point for 5 day forecast
 
   // var weatherImg = $("<img>");
