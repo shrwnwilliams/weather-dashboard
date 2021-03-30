283cd45665768724d8f09f9e04e8069767b44ae6
DINNERman <shrwnwilliams@gmail.com>
2021-03-30 16:06:17 -0400

    worked with tutor to save searched cities into local storage and to rerender the listed searched cities weather data on click

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,76 +1,101 @@
-var userCityInput = $("#city-search-input");
-var formInputEl = $("#city-search");
-var todaysDateEl = $("#todays-date");
-var weatherDisplay = $("#weather-display");
-var currentCity = $("#city-name");
-var currentTemp = $("#current-temperature");
-var currentHumidity = $("#current-humidity");
-var currentWind = $("#current-wind");
-var currentUvIndex = $("#current-uv");
-var pastSearchList = $("#past-searches");
-var today = moment();
+$(document).ready(function () {
+  var userCityInput = $("#city-search-input");
+  var formInputEl = $("#city-search");
+  var todaysDateEl = $("#todays-date");
+  var weatherDisplay = $("#weather-display");
+  var currentCity = $("#city-name");
+  var currentTemp = $("#current-temperature");
+  var currentHumidity = $("#current-humidity");
+  var currentWind = $("#current-wind");
+  var currentUvIndex = $("#current-uv");
+  var pastSearchList = $("#past-searches");
+  var today = moment();
+  var searchHistory = JSON.parse(localStorage.getItem("history")) || [];
 
-todaysDateEl.text(today.format("l"));
+  todaysDateEl.text(today.format("l"));
 
-function getLatLong() {
-  var requestUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    userCityInput.val() +
-    "&units=imperial&appid=49e97d128aa0b52b9299b5b1b5a52107";
+  function getLatLong(city) {
+    var requestUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&appid=49e97d128aa0b52b9299b5b1b5a52107";
 
-  fetch(requestUrl)
-    .then(function (response) {
-      if (response.ok) return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      console.log(data.coord.lat);
-      console.log(data.coord.lon);
-      getCityWeather(data.coord.lat, data.coord.lon);
-    });
-}
+    fetch(requestUrl)
+      .then(function (response) {
+        if (response.ok) return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        currentCity.text(data.name);
+        // console.log(data.coord.lat);
+        // console.log(data.coord.lon);
+        getCityWeather(data.coord.lat, data.coord.lon);
+        if (searchHistory.indexOf(city) === -1) {
+          searchHistory.push(city);
+          localStorage.setItem("history", JSON.stringify(searchHistory));
+          saveCity(city);
+        }
+      });
+  }
+  // data:name
 
-var getCityWeather = function (lat, lon) {
-  var requestWeatherUrl =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-    lat +
-    "&lon=" +
-    lon +
-    "&units=imperial&appid=49e97d128aa0b52b9299b5b1b5a52107";
+  var getSearchValue = function () {
+    var searchValue = userCityInput.val();
+    getLatLong(searchValue);
+    saveCity(searchValue);
+  };
 
-  fetch(requestWeatherUrl)
-    .then(function (response) {
-      if (response.ok) return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      currentTemp.text(data.current.temp + "°F");
-      currentHumidity.text(data.current.humidity + "%");
-      currentWind.text(data.current.wind_speed + " mph");
-      if (data.current.uvi >= 7) {
-        currentUvIndex.addClass("btn btn-danger");
-      } else if (data.current.uvi <= 2) {
-        currentUvIndex.addClass("btn btn-success");
-      } else {
-        currentUvIndex.addClass("btn btn-warning");
-      }
-      currentUvIndex.text(data.current.uvi);
-    });
-};
+  var saveCity = function (searchValue) {
+    var searchItem = $("<li>").text(searchValue).addClass("list-group-item");
+    console.log(searchItem);
+    pastSearchList.append(searchItem);
+    userCityInput.val("");
+  };
+  // searchItem.id = searchValue;
+  pastSearchList.on("click", "li", function(){
+    getLatLong($(this).text());
+  })
 
-formInputEl.on("submit", function (event) {
-  event.preventDefault();
-  getLatLong();
-  weatherDisplay.removeClass("d-none");
-  currentCity.text(userCityInput.val());
-  var searchItem = $("<a>");
-  searchItem.addClass("list-group-item");
-  searchItem.attr("href", "https://google.com");
-  searchItem.text(userCityInput.val() + "\n");
-  pastSearchList.append(searchItem);
+  var getCityWeather = function (lat, lon) {
+    var requestWeatherUrl =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&units=imperial&exclude=hourly,minutely&appid=49e97d128aa0b52b9299b5b1b5a52107";
+
+    fetch(requestWeatherUrl)
+      .then(function (response) {
+        if (response.ok) return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        currentTemp.text(data.current.temp + "°F");
+        currentHumidity.text(data.current.humidity + "%");
+        currentWind.text(data.current.wind_speed + " mph");
+        if (data.current.uvi >= 7) {
+          currentUvIndex.addClass("btn btn-danger");
+        } else if (data.current.uvi <= 2) {
+          currentUvIndex.addClass("btn btn-success");
+        } else {
+          currentUvIndex.addClass("btn btn-warning");
+        }
+        currentUvIndex.text(data.current.uvi);
+      });
+  };
+
+  formInputEl.on("submit", function (event) {
+    event.preventDefault();
+    // getLatLong();
+    getSearchValue();
+    weatherDisplay.removeClass("d-none");
+  });
+
+  // storing past search list items into local storage
+  // when clicking on the lists in the bar, the weather will change to those cities
+  // finding different end point for 5 day forecast
+
+  // var weatherImg = $("<img>");
+  // weatherImg.attr("src", "http://openweathermap.org/img/wn/" + data.daily[0].weather[0].icon +"@2x.png");
+  // weatherDisplay.append(weatherImg);
 });
-
-// Add the searched terms to a list underneath the search bar
-// when clicking on the lists in the bar, the weather will change to those cities
-// storing past search list items into local storage
-// finding different end point for 5 day forecast
